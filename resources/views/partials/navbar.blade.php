c56ec20a50dd7487971118c51a5365fd66d5d73b
whitehatforhire <stephensm2011@gmail.com>
2018-03-28 19:36:25 -0400

    added reviews page

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,4 @@
+<ul class="nav nav-tabs">
+    <li role="presentation"><a href="/reviews">Reviews</a></li>
+    <li role="presentation"><a href="/create">Create Review</a></li>
+  </ul>
--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -3,51 +3,46 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-6 col-lg-offset-3">
-        <h1>CONTENT</h1>
+        <h1>REVIEWS</h1>
       </div>
     </div>
   </div>
   @foreach($reviews as $review)
-    
   <div class="container">
     <div class="row">
       <div class="col-lg-6 col-lg-offset-3">
-        <div class="panel panel-primary">
-            <!-- Default panel contents -->
-            <div class="panel-heading">{{$review->title}}</div>
-            <div class="panel-body">
-              @for($i = 0; $i < $review->overallScore; $i++)
-                <i class="fas fa-star"></i>
-              @endfor
-            </div>
-            <!-- Table -->
-            <table class="table">
-              <tbody>
-                <tr>
-                  <td>{{$review->body}}</td>
-                  <th>Overall Score:</th>
-                  <th>{{$review->overallScore}}</th>
-                </tr>
-                <tr>
-                  <th>Quality: </th>
-                  <tr>
-                    @for($i = 0; $i < $review->qualityScore; $i++)
-                      <i class="fas fa-star"></i>
-                    @endfor
-                  </tr>
-                </tr>
-                  <tr>
-                    <th scope="row">Scores:</th>
-                    <th>Quality: {{$review->qualityScore}}</td>
-                    <th>Personality: {{$review->personalityScore}}</td>
-                    <th>Professionalism: {{$review->professionalismScore}}</th>
-                  </tr>
-              </tbody>
-            </table>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3>
+            @for($i = 0; $i < $review->overallScore; $i++)
+            <i class="fas fa-star"></i>
+            @endfor
+            </h3>
+            <h3>{{$review->title}}</h3>
+            <hr>
+            <p>{{$review->body}}</p>
+            <h4>Quality:</h4>
+            <h4>
+            @for($i = 0; $i < $review->qualityScore; $i++)
+              <i class="fas fa-star"></i>
+            @endfor
+            </h4>
+            <h4>Personality:</h4>
+            <h4>
+            @for($i = 0; $i < $review->personalityScore; $i++)
+              <i class="fas fa-star"></i>
+            @endfor
+            </h4>
+            <h4>Professionalism:</h4>
+            <h4>
+            @for($i = 0; $i < $review->professionalismScore; $i++)
+              <i class="fas fa-star"></i>
+            @endfor
+            </h4>
           </div>
+        </div>
       </div>
     </div>
-    
   </div>
   
   @endforeach
